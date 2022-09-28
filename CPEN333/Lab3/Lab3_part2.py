fe0c034415e639891fe0786b22f2cf3a04d11ebf
brendanlai <laibrendan8@gmail.com>
2022-09-27 17:53:55 -0700

    Add Part2

---
+++ CPEN333/Lab3/Lab3_part2.py
@@ -0,0 +1,128 @@
+# Student name: Brendan Lai
+# Student number: 19241173
+
+import multiprocessing
+
+def checkColumn(puzzle: list, column: int):
+    """ 
+        param puzzle: a list of lists containing the puzzle 
+        param column: the column to check (a value between 0 to 8)
+
+        This function checks the indicated column of the puzzle, and 
+        prints whether it is valid or not. 
+        
+        As usual, this function must not mutate puzzle 
+    """
+    colSet = set()
+
+    for row in puzzle:
+        val = row[column] # get val from specific col in row
+        if val in colSet:
+            print(f"Column {column} not valid")
+            return # Exit the method if subgrid is invalid
+
+        colSet.add(column)
+
+    print(f"Column {column} valid")
+        
+
+def checkRow(puzzle: list, row: int):
+    """ 
+        param puzzle: a list of lists containing the puzzle 
+        param row: the row to check (a value between 0 to 8)
+
+        This function checks the indicated row of the puzzle, and 
+        prints whether it is valid or not. 
+        
+        As usual, this function must not mutate puzzle 
+    """
+    rowSet = set()
+
+    for x in puzzle[row]:
+        if x in rowSet:
+            print(f"Row {row} not valid")
+            return # Exit the method if subgrid is invalid
+        rowSet.add(x)
+
+    print(f"Row {row} valid")
+
+
+def checkSubgrid(puzzle: list, subgrid: int):
+    """ 
+        param puzzle: a list of lists containing the puzzle 
+        param subgrid: the subgrid to check (a value between 0 to 8)
+        Subgrid numbering order:    0 1 2
+                                    3 4 5
+                                    6 7 8
+        where each subgrid itself is a 3x3 portion of the original list
+        
+        This function checks the indicated subgrid of the puzzle, and 
+        prints whether it is valid or not. 
+        
+        As usual, this function must not mutate puzzle 
+    """
+    rowStart = (subgrid // 3) * 3 # Calculate row as 0 or 3 or 6
+    colStart = (subgrid % 3) * 3 # Calculate col as 0 or 3 or 6
+
+    subgridSet = set()
+
+    for row in puzzle[rowStart: rowStart + 3]: # get the 3 rows of the subgrid
+        for x in row[colStart: colStart + 3]: # get vals from rows for cols in subgrid
+            if x in subgridSet:
+                print(f"Subgrid {subgrid} not valid")
+                return # Exit the method if subgrid is invalid
+
+            subgridSet.add(x)
+
+    print(f"Subgrid {subgrid} valid")
+
+if __name__ == "__main__":
+    test1 = [ [6, 2, 4, 5, 3, 9, 1, 8, 7],
+              [5, 1, 9, 7, 2, 8, 6, 3, 4],
+              [8, 3, 7, 6, 1, 4, 2, 9, 5],
+              [1, 4, 3, 8, 6, 5, 7, 2, 9],
+              [9, 5, 8, 2, 4, 7, 3, 6, 1],
+              [7, 6, 2, 3, 9, 1, 4, 5, 8],
+              [3, 7, 1, 9, 5, 6, 8, 4, 2],
+              [4, 9, 6, 1, 8, 2, 5, 7, 3],
+              [2, 8, 5, 4, 7, 3, 9, 1, 6]
+            ]
+    test2 = [ [6, 2, 4, 5, 3, 9 , 1, 8, 7],
+              [5, 1, 9, 7, 2, 8, 6, 3, 4],
+              [8, 3, 7, 6, 1, 4, 2, 9, 5 ],
+              [6, 2, 4, 5, 3, 9 , 1, 8, 7],
+              [5, 1, 9, 7, 2, 8, 6, 3, 4],
+              [8, 3, 7, 6, 1, 4, 2, 9, 5 ],
+              [6, 2, 4, 5, 3, 9 , 1, 8, 7],
+              [5, 1, 9, 7, 2, 8, 6, 3, 4],
+              [8, 3, 7, 6, 1, 4, 2, 9, 5 ]
+            ]
+    
+    testcase = test1   #modify here for other testcases
+    SIZE = 9
+
+    processList = []
+
+    # Create and start processes for each row and append to processList to join
+    for x in range(SIZE):
+        process = multiprocessing.Process(target=checkRow, args=(testcase, x))
+        process.start()
+
+        processList.append(process)
+
+    # Create and start processes for each column and append to processList to join
+    for x in range(SIZE):
+        process = multiprocessing.Process(target=checkColumn, args=(testcase, x))
+        process.start()
+
+        processList.append(process)
+
+    # Create and start processes for each subgrid and append to processList to join
+    for x in range(SIZE):
+        process = multiprocessing.Process(target=checkSubgrid, args=(testcase, x))
+        process.start()
+
+        processList.append(process)
+
+    for x in processList:
+        x.join()
